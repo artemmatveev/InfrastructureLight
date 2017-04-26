3aa67fbd8c186aad1c26b612d12bd57847537298
Artem Matveev <artemmatveev@hotmail.com>
2017-04-26 22:38:13 +0300

    Added extensions class for the "ICustomAttributeProvider"

---
+++ InfrastructureLight Solution/Libs/InfrastructureLight.Common/Extensions/CustomAttributeProviderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace InfrastructureLight.Common.Extensions
+{
+    /// <summary>
+    ///     Extensions class for the <see cref="ICustomAttributeProvider"/>
+    /// </summary>
+    public static class CustomAttributeProviderExtensions
+    {
+        /// <summary>
+        ///     Strongly typed version GetCustomAttributes method
+        /// </summary>        
+        public static T[] GetCustomAttributes<T>(this ICustomAttributeProvider attributeProvider, bool inherit)
+            where T : Attribute
+        {
+            return (T[])attributeProvider.GetCustomAttributes(typeof(T), inherit);
+        }
+    }
+}
