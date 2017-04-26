2eca5d7f02c6cd327b37bbc7bc1a2eeac424b53f
Artem Matveev <artemmatveev@hotmail.com>
2017-04-26 23:24:00 +0300

    Added an ISelectable interface

---
+++ Libs/InfrastructureLight.Domain/ISelectable.cs
@@ -0,0 +1,14 @@
+﻿namespace InfrastructureLite.Domain
+{
+    /// <summary>
+    ///     Describes the selectable item
+    /// </summary>
+    public interface ISelectable
+    {
+        /// <summary>
+        ///     Return <see cref="bool.True" />,
+        ///     if entity selected
+        /// </summary>
+        bool IsSelected { get; }
+    }
+}
