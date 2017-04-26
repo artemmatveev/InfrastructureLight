4b3d3483d8b4a6666ca644c8955178e8937df4a6
Artem Matveev <artemmatveev@hotmail.com>
2017-04-26 23:24:27 +0300

    Added the entity equality comparer

---
+++ Libs/InfrastructureLight.Domain/Comparers/EntityEqualityComparer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLight.Domain.Comparers
+{
+    /// <summary>
+    ///     Entity Equality Comparer
+    /// </summary>
+    /// <typeparam name="TEntity">Entity type</typeparam>
+    public class EntityEqualityComparer<TEntity> : IEqualityComparer<TEntity>
+        where TEntity : class, IEntity
+    {
+        public bool Equals(TEntity x, TEntity y)
+        {
+            if (x == null || y == null)
+                return false;
+
+            return x.Id == y.Id;
+        }
+
+        public int GetHashCode(TEntity obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
