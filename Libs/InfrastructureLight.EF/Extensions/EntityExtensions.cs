ad0d718fef1cae29550be6aad275e34c4fd86566
Artem Matveev <artemmatveev@hotmail.com>
2017-04-27 08:24:46 +0300

    Added extensions class for the Entity

---
+++ Libs/InfrastructureLight.EF/Extensions/EntityExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace InfrastructureLight.EF.Extensions
+{
+    public static class EntityExtensions
+    {
+        /// <summary>
+        ///     Copy data from an entity to an entity
+        /// </summary>
+        public static void CopyDataTo<TEntity>(this TEntity source, TEntity dest) where TEntity : class
+        {
+            var sourceType = source.GetType();
+            var destType = dest.GetType();
+
+            foreach (var info in sourceType.GetProperties())
+            {
+                if (info.Name != "EntityConflict" && info.Name != "EntityState")
+                {
+                    bool isReadOnly = info.GetCustomAttributes(false).Any(x => (x is EditableAttribute)
+                        && (x as EditableAttribute).AllowEdit == false
+                        && (x as EditableAttribute).AllowInitialValue == false);
+
+                    var secondInfo = destType.GetProperty(info.Name);
+
+                    if (!isReadOnly && secondInfo != null && secondInfo.CanWrite)
+                    {
+                        secondInfo.SetValue(dest, info.GetValue(source, null), null);
+                    }
+                }
+            }
+        }
+    }
+}
