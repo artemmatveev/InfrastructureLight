68be76ac198e5a6d265c6368328003304adb8022
Artem Matveev <artemmatveev@hotmail.com>
2017-05-02 08:38:47 +0300

    Added the SQL ConnectionFactory

---
+++ Libs/InfrastructureLight.SqlServer/Factory/ConnectionFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Global.Security;
+
+namespace InfrastructureLight.SqlServer.Factory
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        #region Fields
+
+        public string DataBaseName { get; private set; }
+        public string ConnectionString { get; private set; }
+
+        #endregion
+
+        public ConnectionFactory(string cryptKey)
+        {
+            var cryptConnection = ConfigurationManager.ConnectionStrings["CryptConnectionString"];
+            var dbName = ConfigurationManager.ConnectionStrings["DataBaseName"];
+
+            if (cryptConnection != null)
+            {
+                var encryptConnection = new SqlConnectionStringBuilder(Encrypter.DecryptString(cryptConnection.ConnectionString, cryptKey));
+                DataBaseName = (dbName == null) ? string.Empty : dbName.ConnectionString;
+
+                if (!string.IsNullOrEmpty(DataBaseName))
+                {
+                    encryptConnection.InitialCatalog = DataBaseName;
+                }
+
+                ConnectionString = encryptConnection.ConnectionString;
+            }
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            SqlConnection sqlConnection = string.IsNullOrEmpty(ConnectionString)
+                ? new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
+                : new SqlConnection(ConnectionString);
+
+            return sqlConnection;
+        }
+    }
+}
---
+++ Libs/InfrastructureLight.SqlServer/Factory/IConnectionFactory.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace InfrastructureLight.SqlServer
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection CreateConnection();
+
+        string DataBaseName { get; }
+        string ConnectionString { get; }
+    }
+}
