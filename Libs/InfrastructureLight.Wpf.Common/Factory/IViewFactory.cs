88e5813c4c2d908c3add15f9fc5e018a058769ff
Artem Matveev <artemmatveev@hotmail.com>
2017-05-04 23:06:31 +0300

    Added the ViewFactory

---
+++ Libs/InfrastructureLight.Wpf.Common/Factory/IViewFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace InfrastructureLight.Wpf.Common.Factory
+{
+    public interface IViewFactory
+    {
+        FrameworkElement CreateView<TViewModel>(TViewModel viewModel);
+    }
+}
---
+++ Libs/InfrastructureLight.Wpf.Common/Factory/ViewFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace InfrastructureLight.Wpf.Common.Factory
+{
+    public class ViewFactory : IViewFactory
+    {
+        public FrameworkElement CreateView<TViewModel>(TViewModel viewModel)
+        {
+            Type viewModelType = viewModel.GetType();
+            string typeName = viewModelType.FullName;
+            typeName = typeName.Replace("ViewModel", "View");
+
+            Type viewType = Type.GetType(typeName);
+            if (viewType == null)
+                throw new ArgumentException(
+                    string.Format("Unable to find view type: {0} for given view model.", typeName), "viewModel");
+
+            var view = (FrameworkElement)Activator.CreateInstance(viewType);
+            view.DataContext = viewModel;
+
+            return view;
+        }
+    }
+}
