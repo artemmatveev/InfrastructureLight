21571dfed19b07ffffd1a225d2feeb28fed35cf6
Artem Matveev <artemmatveev@hotmail.com>
2017-05-08 13:30:35 +0300

    Added the EnumToIEnumerable markup extension

---
+++ Libs/InfrastructureLight.Wpf.Common/MarkupExtensions/EnumToIEnumerable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Linq;
+
+using InfrastructureLight.Common.Extensions;
+
+namespace nfrastructureLight.Wpf.Common.MarkupExtensions
+{    
+    public class EnumToIEnumerable : MarkupExtension
+    {
+        private readonly Type _type;
+        public EnumToIEnumerable(Type type)
+        {
+            _type = type;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enum.GetValues(_type)
+                .Cast<object>()
+                .Select(e => new { Value = (int)e, Description = ((Enum)e).GetDescription() });
+        }
+    }
+}
