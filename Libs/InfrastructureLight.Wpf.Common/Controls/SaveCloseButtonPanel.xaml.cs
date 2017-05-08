649ecf8060eeb504ac933fae529d7daf0f91bcf8
Artem Matveev <artemmatveev@hotmail.com>
2017-05-08 13:32:00 +0300

    Added the SaveCloseButtonPanel for regular use

---
+++ Libs/InfrastructureLight.Wpf.Common/Controls/SaveCloseButtonPanel.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Controls;
+
+namespace InfrastructureLight.Wpf.Common.Controls
+{    
+    public partial class SaveCloseButtonPanel : UserControl
+    {
+        public SaveCloseButtonPanel()
+        {
+            InitializeComponent();
+        }
+
+        public string SaveText
+        {
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.xSaveButton.Content = value;
+                }
+            }
+        }
+
+        public string CloseText
+        {
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    this.xCloseButton.Content = value;
+                }
+            }
+        }
+    }
+}
