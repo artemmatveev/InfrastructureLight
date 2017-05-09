04e542030e2bd1aaf7fb0d14879d5363ea4d1ef4
Artem Matveev <artemmatveev@hotmail.com>
2017-05-09 14:22:56 +0300

    Updated the Global.Nlog.Config

---
+++ Libs/Global.NLog.Config/NLogConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Xml;
+using System.Reflection;
+
+using NLog;
+using NLog.Config;
+
+namespace Global.NLog.Config
+{
+    public sealed class NLogConfig
+    {
+        public static void ConfigureNLog(string appName, string companyName)
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Global.NLog.Config.NLog.config"))
+            {
+                var xml = new StreamReader(stream).ReadToEnd()
+                    .Replace("@companyName", companyName)
+                    .Replace("@appName", appName);
+
+                using (var sr = new StringReader(xml))
+                {
+                    using (XmlReader xr = XmlReader.Create(sr))
+                    {
+                        var config = new XmlLoggingConfiguration(xr, null);
+                        LogManager.Configuration = config;
+                    }
+                }
+            }
+        }
+    }
+}
