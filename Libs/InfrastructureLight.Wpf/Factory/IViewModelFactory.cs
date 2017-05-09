eb4f13754edd5e0d540e3ce04db5ebb8145984f0
Artem Matveev <artemmatveev@hotmail.com>
2017-05-09 11:42:49 +0300

    Added the IViewModelFactory

---
+++ Libs/InfrastructureLight.Wpf/Factory/IViewModelFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace InfrastructureLight.Wpf.Factory
+{
+    using ViewModels;
+
+    public interface IViewModelFactory
+    {
+        T Get<T>() where T : ViewModelBase;
+        T Create<T>() where T : ViewModelBase;
+    }
+}
