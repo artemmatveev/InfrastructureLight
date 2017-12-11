1b3e356d482af8b6dabd6ab6effedd022a477483
Artem Matveev <artemmatveev@hotmail.com>
2017-12-11 23:34:00 +0300

    Added the ExceptionExtensions

---
+++ Libs/InfrastructureLight.Common/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfrastructureLight.Common.Extensions
+{
+    /// <summary>
+    ///     Extensions class for the <see cref="Exception"/>
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        public static string GetFullErrorInfo(this Exception ex, string exMessage = "", int offset = 0)
+        {
+            if (ex != null)
+            {
+                string strOffset = new string(' ', offset);
+                string indent1 = offset > 0 ? "|_" : "";
+                string indent2 = offset > 0 ? "  " : "";
+
+                exMessage += $"{strOffset}{indent1}{ex.Message}\r\n{strOffset}Type: {ex.GetType().Name}\r\n\r\n" +
+                             $" {indent2}StackTrace:\r\n  {ex.StackTrace}\r\n";
+
+                return ex.InnerException.GetFullErrorInfo(exMessage, offset + 1);
+            }
+            return exMessage;
+        }
+    }
+}
