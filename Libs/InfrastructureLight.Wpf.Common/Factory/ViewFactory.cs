00f4fce5d912a79b0f317ae5e932ca68d4f23bc3
Artem Matveev <artemmatveev@hotmail.com>
2017-12-11 23:37:38 +0300

    Bug fix ViewFactory

--- Libs/InfrastructureLight.Wpf.Common/Factory/ViewFactory.cs
+++ Libs/InfrastructureLight.Wpf.Common/Factory/ViewFactory.cs
@@ -3,18 +3,21 @@ using System.Windows;
 
 namespace InfrastructureLight.Wpf.Common.Factory
 {
+    using System.Reflection;
+
     public class ViewFactory : IViewFactory
     {
         public FrameworkElement CreateView<TViewModel>(TViewModel viewModel)
         {
             Type viewModelType = viewModel.GetType();
-            string typeName = viewModelType.FullName;
-            typeName = typeName.Replace("ViewModel", "View");
 
-            Type viewType = Type.GetType(typeName);
+            string assemblyQualifiedName = viewModelType.AssemblyQualifiedName;
+            assemblyQualifiedName = assemblyQualifiedName.Replace("ViewModel", "View");
+
+            Type viewType = Type.GetType(assemblyQualifiedName);
             if (viewType == null)
                 throw new ArgumentException(
-                    string.Format("Unable to find view type: {0} for given view model.", typeName), "viewModel");
+                    string.Format("Unable to find view type: {0} for given view model.", assemblyQualifiedName), "viewModel");
 
             var view = (FrameworkElement)Activator.CreateInstance(viewType);
             view.DataContext = viewModel;
