84fe3221b51f9877826236586369cbc1546ff1d6
Artem Matveev <artemmatveev@hotmail.com>
2020-02-27 11:45:27 +0300

    Update DomainException.cs

--- Libs/InfrastructureLight.Common/Exceptions/DomainException.cs
+++ Libs/InfrastructureLight.Common/Exceptions/DomainException.cs
@@ -10,6 +10,9 @@ namespace InfrastructureLight.Common.Exceptions
 
         public DomainException(string message)
           : base(message) { }
+          
+        public DomainException(string format, params object[] args) 
+          : base(string.Format(format, args)) { }
 
         public DomainException(string message, Exception innerException)
            : base(message, innerException) { }
